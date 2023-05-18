7c6df7ffe779f5f4bb41a214d431e3f016f4db6b
uthoa1702 <uthoa1702@gmail.com>
2023-05-18 17:18:28 +0700

    update

--- ss1/demo/src/main/java/com/codegym/controller/GreetingController.java
+++ ss1/demo/src/main/java/com/codegym/controller/GreetingController.java
@@ -2,11 +2,15 @@ package com.codegym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
+    @RequestMapping(value = "/greeting/{name}",method = RequestMethod.GET)
+    public String name(Model model,@PathVariable("name") String name){
+        model.addAttribute("name", name);
+        return "index";
+    }
     @GetMapping("/greeting")
     public String greeting(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
